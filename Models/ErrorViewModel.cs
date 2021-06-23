db3ad74ef8854a8172a25c9a938b067932cbd761
Tyler D <tylerd@microsoft.com>
2021-06-23 14:27:52 +0000

    code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace actions_demo_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
